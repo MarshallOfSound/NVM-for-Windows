9bc0d817e7acd020cc8ff851a5bfc0c2b594fdc8
Samuel Attard <samuel.r.attard@gmail.com>
2016-07-24 22:09:21 +1000

    Remove debug variables

--- nvm-windows/Program.cs
+++ nvm-windows/Program.cs
@@ -10,14 +10,12 @@ namespace nvm_windows
     {
         static int Main(string[] args)
         {
-            string[] tmp = { "use", "6" };
             var result = Parser.Default.ParseArguments<ListOptions, InstallOptions, UseOptions, LinkOptions>(args)
                 .WithParsed<ListOptions>(opts => List.Run(opts))
                 .WithParsed<InstallOptions>(opts => Install.Run(opts))
                 .WithParsed<UseOptions>(opts => Use.Run(opts))
                 .WithParsed<LinkOptions>(opts => Link.Run(opts))
                 .WithNotParsed(errs => Console.WriteLine(errs));
-            // Console.ReadLine();
             return 0;
         }
     }
